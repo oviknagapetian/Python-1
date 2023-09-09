7b8d3da54de38b48de96c142eba746b087363c47
oviknagapetian <oviknagapetian@gmail.com>
2023-09-09 19:06:12 +0300

    добавил исключения

--- notes.py
+++ notes.py
@@ -7,15 +7,23 @@ NOTES_FILE = "notes.json"
 
 # Проверка существования файла заметок и его загрузка, если он существует
 def load_notes():
-    if os.path.exists(NOTES_FILE):
-        with open(NOTES_FILE, "r") as file:
-            return json.load(file)
-    return []
+    try:
+        if os.path.exists(NOTES_FILE):
+            with open(NOTES_FILE, "r") as file:
+                return json.load(file)
+        else:
+            return []
+    except Exception as e:
+        print(f"Ошибка при загрузке заметок: {e}")
+        return []
 
 # Сохранение заметок в файл
 def save_notes(notes):
-    with open(NOTES_FILE, "w") as file:
-        json.dump(notes, file, indent=4)
+    try:
+        with open(NOTES_FILE, "w") as file:
+            json.dump(notes, file, indent=4)
+    except Exception as e:
+        print(f"Ошибка при сохранении заметок: {e}")
 
 # Создание новой заметки
 def create_note():
@@ -27,87 +35,102 @@ def create_note():
 
 # Добавление заметки
 def add_note():
-    notes = load_notes()
-    note = create_note()
-    notes.append(note)
-    save_notes(notes)
-    print("Заметка успешно сохранена.")
+    try:
+        notes = load_notes()
+        note = create_note()
+        notes.append(note)
+        save_notes(notes)
+        print("Заметка успешно сохранена.")
+    except Exception as e:
+        print(f"Ошибка при добавлении заметки: {e}")
 
 # Вывод списка заметок с фильтрацией по дате
 def list_notes():
-    notes = load_notes()
-    if not notes:
-        print("Список заметок пуст.")
-        return
+    try:
+        notes = load_notes()
+        if not notes:
+            print("Список заметок пуст.")
+            return
 
-    print("Список заметок:")
-    for i, note in enumerate(notes, 1):
-        print(f"{i}. Заголовок: {note['title']}")
-        print(f"   Дата создания: {note['timestamp']}")
-        print(f"   Содержание: {note['message']}\n")
+        print("Список заметок:")
+        for i, note in enumerate(notes, 1):
+            print(f"{i}. Заголовок: {note['title']}")
+            print(f"   Дата создания: {note['timestamp']}")
+            print(f"   Содержание: {note['message']}\n")
+    except Exception as e:
+        print(f"Ошибка при выводе списка заметок: {e}")
 
 # Редактирование заметки
 def edit_note():
-    notes = load_notes()
-    if not notes:
-        print("Список заметок пуст.")
-        return
+    try:
+        notes = load_notes()
+        if not notes:
+            print("Список заметок пуст.")
+            return
 
-    list_notes()
-    choice = int(input("Выберите номер заметки для редактирования: ")) - 1
+        list_notes()
+        choice = int(input("Выберите номер заметки для редактирования: ")) - 1
 
-    if 0 <= choice < len(notes):
-        new_title = input("Введите новый заголовок: ")
-        new_message = input("Введите новый текст: ")
-        notes[choice]["title"] = new_title
-        notes[choice]["message"] = new_message
-        notes[choice]["timestamp"] = datetime.now().isoformat()
-        save_notes(notes)
-        print("Заметка успешно отредактирована.")
-    else:
-        print("Неверный выбор заметки.")
+        if 0 <= choice < len(notes):
+            new_title = input("Введите новый заголовок: ")
+            new_message = input("Введите новый текст: ")
+            notes[choice]["title"] = new_title
+            notes[choice]["message"] = new_message
+            notes[choice]["timestamp"] = datetime.now().isoformat()
+            save_notes(notes)
+            print("Заметка успешно отредактирована.")
+        else:
+            print("Неверный выбор заметки.")
+    except Exception as e:
+        print(f"Ошибка при редактировании заметки: {e}")
 
 # Удаление заметки
 def delete_note():
-    notes = load_notes()
-    if not notes:
-        print("Список заметок пуст.")
-        return
+    try:
+        notes = load_notes()
+        if not notes:
+            print("Список заметок пуст.")
+            return
 
-    list_notes()
-    choice = int(input("Выберите номер заметки для удаления: ")) - 1
+        list_notes()
+        choice = int(input("Выберите номер заметки для удаления: ")) - 1
 
-    if 0 <= choice < len(notes):
-        deleted_note = notes.pop(choice)
-        save_notes(notes)
-        print("Заметка успешно удалена.")
-    else:
-        print("Неверный выбор заметки.")
+        if 0 <= choice < len(notes):
+            deleted_note = notes.pop(choice)
+            save_notes(notes)
+            print("Заметка успешно удалена.")
+        else:
+            print("Неверный выбор заметки.")
+    except Exception as e:
+        print(f"Ошибка при удалении заметки: {e}")
 
 # Основной цикл приложения
 def main():
-    while True:
-        print("\nМеню:")
-        print("1. Создать новую заметку")
-        print("2. Список заметок")
-        print("3. Редактировать заметку")
-        print("4. Удалить заметку")
-        print("5. Выход")
-        choice = input("Выберите опцию: ")
+    try:
+        while True:
+            print("\nМеню:")
+            print("1. Создать новую заметку")
+            print("2. Список заметок")
+            print("3. Редактировать заметку")
+            print("4. Удалить заметку")
+            print("5. Выход")
+            choice = input("Выберите опцию: ")
 
-        if choice == "1":
-            add_note()
-        elif choice == "2":
-            list_notes()
-        elif choice == "3":
-            edit_note()
-        elif choice == "4":
-            delete_note()
-        elif choice == "5":
-            print("Выход из программы.")
-            break
-        else:
-            print("Неверный выбор. Пожалуйста, выберите корректную опцию.")
+            if choice == "1":
+                add_note()
+            elif choice == "2":
+                list_notes()
+            elif choice == "3":
+                edit_note()
+            elif choice == "4":
+                delete_note()
+            elif choice == "5":
+                print("Выход из программы.")
+                break
+            else:
+                print("Неверный выбор. Пожалуйста, выберите корректную опцию.")
+    except KeyboardInterrupt:
+        print("\nВыход из программы.")
 
 if __name__ == "__main__":
     main()
